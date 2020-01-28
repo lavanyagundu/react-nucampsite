7fd20357f308dd44a24506a783155da041a17486
lavanya <gundu.lavanya@gmail.com>
2020-01-27 23:09:39 -0800

    React Transitions

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { connect} from 'react-redux';
 import About from './AboutComponent';
 import { postComment,fetchCampsites,fetchComments, fetchPromotions } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const mapDispatchToProps = { 
   
@@ -68,6 +69,8 @@ class Main extends Component {
         
           <div className="App">
                 <Header />
+                <TransitionGroup>
+                <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={()=> <Directory  campsites={this.props.campsitess} />} />
@@ -76,7 +79,8 @@ class Main extends Component {
                     <Route exact path='/aboutus' render={() => <About partners={this.props.partners} />} />
                     <Redirect to='/home' />
               </Switch>
-        
+              </CSSTransition>
+              </TransitionGroup>
                <Footer />
           </div>
       );
